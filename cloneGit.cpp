bc83127119eff4211ffc5a83777b64f2169cf8e7
joseandery <joseandery2201@gmail.com>
2026-03-17 08:47:21 -0300

    Mudanca_Andery_RepoDaniel

--- cloneGit.cpp
+++ cloneGit.cpp
@@ -14,7 +14,7 @@ int main() {
     cout << "\033[1;32m*   \033[1;33m PARABÉNS 34DS!                     \033[1;32m          *\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;36mVocê clonou um repositório público do Git     \033[1;32m*\033[0m\n";
-    cout << "\033[1;32m*   \033[1;36mpara o seu repositório local!                 \033[1;32m*\033[0m\n";
+    cout << "\033[1;32m*   \033[1;36mDaniel, obrigado por participar! Ass: Andery!                 \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
     cout << "\033[1;32m*   \033[1;33m'Yay :CloverDance:  -Daniel'                 \033[1;32m*\033[0m\n";
     cout << "\033[1;32m*                                                 *\033[0m\n";
